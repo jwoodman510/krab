b3d0f093f1a9d81e4b7a6153d2193931960f58f1
Jordan Woodman <jordan.woodman@ignitionone.com>
2016-09-20 20:48:55 -0400

    Added caching

---
+++ Krab/Krab.DataAccess/User/CachedUser.cs
@@ -0,0 +1,19 @@
+﻿namespace Krab.DataAccess.User
+{
+    public class CachedUser
+    {
+        public string Id { get; set; }
+        public int UserId { get; set; }
+
+        public CachedUser(AspNetUser aspNetUser)
+        {
+            Id = aspNetUser.Id;
+            UserId = aspNetUser.UserId;
+        }
+
+        public CachedUser()
+        {
+            
+        }
+    }
+}
--- Krab/Krab.Web/Controllers/HomeController.cs
+++ Krab/Krab.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using Krab.Cache;
+using Krab.DataAccess.Dac;
+using Krab.DataAccess.User;
 using Microsoft.AspNet.Identity;
 
 namespace Krab.Web.Controllers
@@ -8,16 +10,21 @@ namespace Krab.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ICache _cache;
-        public HomeController(ICache cache)
+        private readonly IUserDac _userDac;
+
+        public HomeController(ICache cache, IUserDac userDac)
         {
             _cache = cache;
+            _userDac = userDac;
         }
 
         public ActionResult Index()
         {
             var userId = User.Identity.GetUserId();
 
+            var user = _userDac.Get(userId);
 
+            _cache.SetValue(userId, new CachedUser(user), 60 * 5);
 
             return View();
         }
