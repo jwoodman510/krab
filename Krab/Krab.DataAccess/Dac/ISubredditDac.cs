780e07d7928a0a5f6a3a0e69579358d49b925cdc
Jordan Woodman <jordan.woodman@ignitionone.com>
2016-10-10 15:12:10 -0400

    Added subreddit dac

---
+++ Krab/Krab.DataAccess/Dac/ISubredditDac.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Krab.DataAccess.Subreddit;
+
+namespace Krab.DataAccess.Dac
+{
+    public interface ISubredditDac
+    {
+        IEnumerable<Subreddit.Subreddit> GetByKeywordResponseSetId(int id);
+    }
+
+    public class SubredditDac : ISubredditDac
+    {
+        private readonly SubredditDb _subredditDb;
+
+        public SubredditDac(SubredditDb subredditDb)
+        {
+            _subredditDb = subredditDb;
+        }
+
+        public IEnumerable<Subreddit.Subreddit> GetByKeywordResponseSetId(int id)
+        {
+            return _subredditDb.Subreddits
+                .AsNoTracking()
+                .Where(s => s.KeywordResponseSetId == id);
+        }
+    }
+}
