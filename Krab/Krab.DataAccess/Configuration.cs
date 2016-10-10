8a84e315fdb8c6fc5b2b266f0f49cb03a4c70703
Jordan Woodman <jordan.woodman@ignitionone.com>
2016-10-10 14:13:12 -0400

    Added SubredditDb

--- Krab/Krab.DataAccess/Configuration.cs
+++ Krab/Krab.DataAccess/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿using Krab.DataAccess.Dac;
 using Krab.DataAccess.KeywordResponseSet;
 using Krab.DataAccess.RedditUser;
+using Krab.DataAccess.Subreddit;
 using Krab.DataAccess.User;
 using Microsoft.Practices.Unity;
 
@@ -13,6 +14,7 @@ namespace Krab.DataAccess
             container.RegisterType<UserDb, UserDb>();
             container.RegisterType<KeywordResponseSetsDb, KeywordResponseSetsDb>();
             container.RegisterType<RedditUserDb, RedditUserDb>();
+            container.RegisterType<SubredditDb, SubredditDb>();
 
             container.RegisterType<IUserDac, UserDac>();
             container.RegisterType<IKeywordResponseSetDac, KeywordResponseSetDac>();
