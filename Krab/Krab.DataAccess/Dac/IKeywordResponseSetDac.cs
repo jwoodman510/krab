cf73dbd6f65dc1febf56b0ac13e99d7976393694
Jordan Woodman <jordan.woodman@ignitionone.com>
2016-10-17 09:56:45 -0400

    Add delete and update keyword-response set endpoints

--- Krab/Krab.DataAccess/Dac/IKeywordResponseSetDac.cs
+++ Krab/Krab.DataAccess/Dac/IKeywordResponseSetDac.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using Krab.DataAccess.Exception;
 using Krab.DataAccess.KeywordResponseSet;
 
 namespace Krab.DataAccess.Dac
@@ -10,7 +11,10 @@ namespace Krab.DataAccess.Dac
         IEnumerable<KeywordResponseSet.KeywordResponseSet> GetAll();
         IEnumerable<KeywordResponseSet.KeywordResponseSet> GetByUserId(int userId);
         KeywordResponseSet.KeywordResponseSet Get(int id);
-        //KeywordResponseSet.KeywordResponseSet DeleteByUserId(int id);
+
+        KeywordResponseSet.KeywordResponseSet Update(KeywordResponseSet.KeywordResponseSet set);
+
+        void Delete(IEnumerable<int> ids);
     }
 
     public class KeywordResponseSetDac : IKeywordResponseSetDac
@@ -39,6 +43,54 @@ namespace Krab.DataAccess.Dac
         {
             return _keywordResponseSetsDb.KeywordResponseSets.Find(id);
         }
+        
+        public KeywordResponseSet.KeywordResponseSet Update(KeywordResponseSet.KeywordResponseSet set)
+        {
+            if(set.Id < 1)
+                throw new ValidationException("Invalid KeywordResponseSet Id.");
 
+            if (string.IsNullOrWhiteSpace(set.Keyword))
+                throw new ValidationException("Missing Keyword.");
+
+            if (string.IsNullOrWhiteSpace(set.Status))
+                throw new ValidationException("Missing Response.");
+
+            if(set.StatusId != (int)KeywordResponseSetStatus.Active && set.StatusId != (int)KeywordResponseSetStatus.Paused)
+                throw new ValidationException("Invalid Status.");
+
+            var previous = Get(set.Id);
+
+            if(previous == null)
+                throw new NotFoundException("Not Found.");
+
+            var entry = _keywordResponseSetsDb.Entry(previous);
+
+            entry.Entity.Keyword = set.Keyword;
+            entry.Entity.Response = set.Response;
+            entry.Entity.StatusId = set.StatusId;
+
+            _keywordResponseSetsDb.SaveChanges();
+
+            _keywordResponseSetsDb.Entry(previous).State = EntityState.Detached;
+
+
+            return set;
+        }
+
+        public void Delete(IEnumerable<int> ids)
+        {
+            foreach (var id in ids)
+            {
+                var existing = _keywordResponseSetsDb.KeywordResponseSets.Find(id);
+
+                if (existing == null)
+                    return;
+
+                var entry = _keywordResponseSetsDb.Entry(existing);
+                entry.State = EntityState.Deleted;
+
+                _keywordResponseSetsDb.SaveChanges();
+            }
+        }
     }
 }
