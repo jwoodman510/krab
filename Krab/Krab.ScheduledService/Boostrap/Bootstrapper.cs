7ae41ace49ffb6e3ffdeebb405ae790eb9981343
Jordan Woodman <jordan.woodman@live.com>
2016-10-21 19:07:43 -0400

    Modified scheduled service to use topshelf for deployment

--- Krab/Krab.ScheduledService/Boostrap/Bootstrapper.cs
+++ Krab/Krab.ScheduledService/Boostrap/Bootstrapper.cs
@@ -1,6 +1,9 @@
-﻿using log4net;
+﻿using Krab.ScheduledService.Jobs;
+using log4net;
 using Microsoft.Practices.ServiceLocation;
 using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
 
 namespace Krab.ScheduledService.Boostrap
 {
@@ -10,9 +13,9 @@ namespace Krab.ScheduledService.Boostrap
         {
             var container = new UnityContainer();
 
-            RegisterInstances(container);
-
             ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(container));
+
+            RegisterInstances(container);
         }
 
         private static void RegisterInstances(IUnityContainer container)
@@ -24,6 +27,43 @@ namespace Krab.ScheduledService.Boostrap
             Caching.Configuration.Register(container);
             Api.Configuration.Register(container);
             Global.Configuration.Register(container);
+
+            TryGetInstances();
+        }
+
+        private static void TryGetInstances()
+        {
+            var locator = ServiceLocator.Current;
+            
+            if (locator == null)
+            {
+                throw new Exception("Unable to find Service Locator!");
+            }
+
+            var logger = locator.GetInstance<ILog>();
+
+            var types = new List<Type>
+            {
+                typeof(IDeleteLogs),
+                typeof(IProcessKeywordResponseSets)
+            };
+
+            foreach (var type in types)
+            {
+                try
+                {
+                    locator.GetInstance(type);
+                }
+                catch (Exception ex)
+                {
+                    logger.Warn($"Unable to find instane of {type.Name}!");
+                    logger.Error($"Unable to find instane of {type.Name}!", ex);
+
+#if DEBUG
+                    throw;
+#endif
+                }
+            }
         }
     }
 }
