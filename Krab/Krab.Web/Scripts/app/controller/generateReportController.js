c8d72fbfc4dd46a11f5971ce6d942e2a73eb7937
Jordan Woodman <jordan.woodman@live.com>
2016-11-05 18:00:18 -0400

    Added report modal

--- Krab/Krab.Web/Scripts/app/controller/generateReportController.js
+++ Krab/Krab.Web/Scripts/app/controller/generateReportController.js
@@ -3,11 +3,57 @@
     .controller("generateReportController", generateReportController);
 
 function generateReportController($scope, modalService) {
+
+    $scope.format = "dd-MMMM-yyyy";
+
+    function init() {
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        $scope.endDate = yesterday;
+        $scope.startDate = yesterday;
+    };
+
+    init();
+
+    $scope.startDatePopup = {
+        opened: false
+    };
+
+    $scope.openStartDate = function () {
+        $scope.startDatePopup.opened = true;
+    };
+
+    $scope.endDatePopup = {
+        opened: false
+    };
+
+    $scope.openEndDate = function () {
+        $scope.endDatePopup.opened = true;
+    };
+
     $scope.submit = function () {
         modalService.close();
     }
 
     $scope.cancel = function () {
         modalService.close();
+    }
+
+    $scope.isValid = function() {
+        var today = new Date();
+        var minDate = new Date();
+        minDate.setDate(minDate.getDate() - 368);
+
+        if (!$scope.endDate || $scope.endDate === null || $scope.endDate === "") {
+            return false;
+        }
+
+        if (!$scope.startDate || $scope.startDate === null || $scope.startDate === "") {
+            return false;
+        }
+
+        return ($scope.endDate <= today) &&
+               ($scope.endDate >= $scope.startDate &&
+               ($scope.startDate >= minDate));
     }
 }
